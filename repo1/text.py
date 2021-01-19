5d1404387496599f0dec84faca85ea581c003464
pony71909 <10746026@ntub.edu.tw>
2021-01-19 11:58:10 +0800

    Signed-off-by: pony71909 <10746026@ntub.edu.tw>

--- repo1/text.py
+++ repo1/text.py
@@ -3,6 +3,13 @@ print("happy new year")
 print("dev test")
 print("本地端做修改")
 print("本地端做改動4")
+print("本地端合併4")
+print("本地端合併5")
+print("本地端合併4")
+print("本地端合併5")
+print("本地端合併4")
+print("本地端合併5")
+
 
 
 
