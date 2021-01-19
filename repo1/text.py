df35bde795b85f51d87ef5f51a0e94495fa68690
pony71909 <10746026@ntub.edu.tw>
2021-01-19 11:32:45 +0800

    123

--- repo1/text.py
+++ repo1/text.py
@@ -1,5 +1,10 @@
 print("hello git test twice222")
 print("happy new year")
+print("happy new year")
+print("happy new year")
+print("happy new year")
+print("happy new year")
+print("happy new year")
 
 
 
