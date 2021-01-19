22535a6264a053d6e1a920108c3ebddf0b38d9be
pony71909 <10746026@ntub.edu.tw>
2021-01-19 11:23:16 +0800

    我加了四行how to do it

--- repo1/text.py
+++ repo1/text.py
@@ -1,5 +1,10 @@
 print("hello git test twice222")
 print("happy new year")
 print("hi")
+print('how to do it!')
+print('how to do it!')
+print('how to do it!')
+print('how to do it!')
+
 
 # hi this is comment
