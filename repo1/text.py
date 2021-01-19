00cd7c56e799b9bb3a971173d31b0e61f4b03f02
pony71909 <10746026@ntub.edu.tw>
2021-01-19 11:25:05 +0800

    刪除

--- repo1/text.py
+++ repo1/text.py
@@ -2,9 +2,7 @@ print("hello git test twice222")
 print("happy new year")
 print("hi")
 print('how to do it!')
-print('how to do it!')
-print('how to do it!')
-print('how to do it!')
+
 
 
 # hi this is comment
